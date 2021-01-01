e7ef3ee407c01f4c67d96c47368f4fea4ee397d1
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-01-01 15:21:14 -0600

    creates BufferedWriter object from filename and later closes it

--- src/com/hervey/app/CreateFileApp.java
+++ src/com/hervey/app/CreateFileApp.java
@@ -1,10 +1,48 @@
 package com.hervey.app;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+•	Read the file name from the console.
+•	Create and use a BufferedWriter object.
+•	Do not read anything from the file system.
+•	Read lines from the console until the user enters "exit".
+•	Write absolutely all of the entered lines (including "exit") to the file, each on a new line.
+•	The main method should close the BufferedWriter object after using it
+
+*/
+
 public class CreateFileApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String fileName = fetchFileName();
+
+		// create BufferedWriter object
+
+		BufferedWriter bufferedWriter = fetchBufferedWriterObject(fileName);
+
+	}
+
+	private static BufferedWriter fetchBufferedWriterObject(String inFileName) {
+		FileWriter writerFile;
+		BufferedWriter createdWriter = null;
+		try {
+			writerFile = new FileWriter(inFileName);
+			createdWriter = new BufferedWriter(writerFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return createdWriter;
+	}
+
+	static String fetchFileName() {
+		// during prototyping, file name is given as literal
+		// later will be collected from console
+		String fileName = "example.txt";
+		return fileName;
 	}
 
 }
